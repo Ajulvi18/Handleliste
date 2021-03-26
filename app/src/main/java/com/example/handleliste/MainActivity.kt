ac4d901305348777d1959e227d85cccdd740d6b6
Andreas Johan Holme Ulvig <ajulvi18@uia.no>
2021-03-26 17:12:36 +0100

    setting up firebase integration

--- app/src/main/java/com/example/handleliste/MainActivity.kt
+++ app/src/main/java/com/example/handleliste/MainActivity.kt
@@ -6,15 +6,20 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.handleliste.data.ListItem
 import com.example.handleliste.databinding.ActivityMainBinding
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.ktx.storage
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var storage: FirebaseStorage
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
+        storage = Firebase.storage
 
         val listOfItems = Datasource( this).getItemList()
 
